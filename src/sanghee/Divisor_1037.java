2646c4f27be6f4a7d823e1f73550aa8e3c2d5aae
woosh1108 <woosh1108@naver.com>
2023-08-17 17:01:13 +0900

    백준 브론즈1 1037 약수

---
+++ src/sanghee/Divisor_1037.java
@@ -0,0 +1,33 @@
+package sanghee;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Divisor_1037 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        if(N<1 | N>50){
+            return;
+        }
+        sc.nextLine();
+
+        int[] M = new int[N];
+        for(int i=0; i<N; i++){
+            M[i] = sc.nextInt();
+            if(M[i]<2 | M[i]>1000000){
+                return;
+            }
+            for(int j=0; j<i; j++){
+                if(M[i]==M[j]){
+                    return;
+                }
+            }
+        }
+
+        Arrays.sort(M);
+
+        System.out.println(M[0]*M[N-1]);
+    }
+}
