b1b3676480a2441f72e9ec4d459b6e9ba58e7a0f
woosh1108 <woosh1108@naver.com>
2023-09-01 17:38:56 +0900

    백준 실버3 1929 소수 구하기

--- src/sanghee/소수구하기_1929.java
+++ src/sanghee/소수구하기_1929.java
@@ -9,20 +9,28 @@ public class 소수구하기_1929 {
         int M = sc.nextInt();
         int N = sc.nextInt();
 
+        // 0 ~ N 까지의 숫자의 소수 여부를 저장할 배열 생성
         boolean[] prime = new boolean[N + 1];
 
-        prime[0] = prime[1] = true;
+        prime[0] = prime[1] = true; // 0과 1은 소수가 아니므로 true로 초기회
 
-        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-            if(prime[i]) continue;
-            for(int j = i * i; j < prime.length; j += i) {
+        // 에라토스테네스의 체 알고리즘을 사용하여 소수 구하기
+        // → 초기에 모든 수를 소수로 가정하고, 소수의 배수를 모두 제거하는 방식
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) { // 배열 길이의 제곱근까지가 소수를 판별하기 위한 최적의 범위
+            for(int j = i * i; j < prime.length; j += i) { // 순차적으로 i 이외의 i의 배수를 제거
                 prime[j] = true;
+            }
+            if(!prime[i]) {
+                break;
             }
         }
 
+        // prime 배열에서 M ~ N 까지의 소수 출력하기
         for(int i = M; i <= N; i++) {
             // false = 소수
-            if(!prime[i]) System.out.println(i);
+            if(!prime[i]){
+                System.out.println(i);
+            }
         }
 
     }
