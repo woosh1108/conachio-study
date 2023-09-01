122f0fb0a244979a00600f41ce4c860be3abbdba
woosh1108 <woosh1108@naver.com>
2023-09-01 20:31:09 +0900

    백준 브론즈1 2309 일곱 난쟁이

--- src/sanghee/일곱난쟁이_2309.java
+++ src/sanghee/일곱난쟁이_2309.java
@@ -7,18 +7,22 @@ public class 일곱난쟁이_2309 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // 난쟁이 키 입력받기
         int[] N = new int[9];
         int sum=0;
         for(int i=0; i<9; i++){
             N[i] = sc.nextInt();
             sum+=N[i];
         }
+
+        // 오름차순으로 출력하기 위해 정렬해준다.
         Arrays.sort(N);
 
+        // 총 합에서 난쟁이 2개를 빼서 100이 되는 것을 찾는다.
         int spy1=0, spy2=0;
         for (int i = 0; i < N.length - 1; i++) {
             for (int j = i + 1; j < N.length; j++) {
-                if (sum - N[i] - N[j] == 100) {
+                if (sum - N[i] - N[j] == 100) { // 만약 100이면 스파이들을 저장해준다.
                     spy1 = i;
                     spy2 = j;
                     break;
@@ -26,6 +30,7 @@ public class 일곱난쟁이_2309 {
             }
         }
 
+        // 배열을 돌며 스파이 난쟁이가 아닌 것들을 제외하고 출력해준다.
         for (int i = 0; i < N.length; i++) {
             if (i != spy1 & i != spy2) {
                 System.out.println(N[i]);
