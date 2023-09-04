91f8ea6c5c7c9d1a08ef8de99f4c53886ce05b6f
woosh1108 <woosh1108@naver.com>
2023-09-04 16:13:46 +0900

    백준 브론즈1 10798 세로읽기

--- src/sanghee/세로읽기_10798.java
+++ src/sanghee/세로읽기_10798.java
@@ -3,7 +3,6 @@ package sanghee;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class 세로읽기_10798 {
     public static void main(String[] args) throws IOException {
