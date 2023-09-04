40f17fb9402c856bf873b3f2e6e6250db16a5a3a
woosh1108 <woosh1108@naver.com>
2023-09-04 16:28:23 +0900

    백준 브론즈3 5086 배수와 약수

--- src/sanghee/배수와약수_5086.java
+++ src/sanghee/배수와약수_5086.java
@@ -10,15 +10,16 @@ public class 배수와약수_5086 {
             int A = sc.nextInt();
             int B = sc.nextInt();
 
+            // A와 B가 0을 입력받았다면 무한루프 깨기
             if(A==0 & B==0){
                 break;
             }
 
-            if(B%A==0) {
+            if(B%A==0) { // A가 B의 약수라면 factor 출력
                 System.out.println("factor");
-            } else if (A%B==0) {
+            } else if (A%B==0) { // A가 B의 배수라면 multiple 출력
                 System.out.println("multiple");
-            }else{
+            }else{ // 둘 다 아니라면 neither 출력
                 System.out.println("neither");
             }
         }
