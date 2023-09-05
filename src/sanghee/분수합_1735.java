8c4c18c045fa58c1c20e248422ef590d7ceefe1f
woosh1108 <woosh1108@naver.com>
2023-09-05 17:18:08 +0900

    백준 실버3 1735 분수 합

---
+++ src/sanghee/분수합_1735.java
@@ -0,0 +1,45 @@
+package sanghee;
+
+import java.util.Scanner;
+
+public class 분수합_1735 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numerator1 = sc.nextInt(); // 분수의 분자1
+        int denominator1 = sc.nextInt(); // 분수의 분모1
+        int numerator2 = sc.nextInt(); // 분수의 분자2
+        int denominator2 = sc.nextInt(); // 분수의 분모2
+
+        // 두 분수를 통분하기
+        int denominator = denominator1*denominator2/GCD(denominator1,denominator2);
+        int numerator = (denominator/denominator1*numerator1)+(denominator/denominator2*numerator2);
+
+        // 통분한 분수를 기약분수로 만들기
+        int a = GCD(numerator,denominator);
+        denominator = denominator/a;
+        numerator = numerator/a;
+
+        System.out.println(numerator+" "+denominator);
+
+    }
+
+    public static int GCD(int a, int b){ // 최대공약수를 구하는 메소드
+        int c = a;
+        int d = b;
+        if(d>c){
+            int e = c;
+            c = d;
+            d = e;
+        }
+
+        while(true){
+            if(c%d==0){
+                return d;
+            }else{
+                int e = c%d;
+                c=d;
+                d=e;
+            }
+        }
+    }
+}
