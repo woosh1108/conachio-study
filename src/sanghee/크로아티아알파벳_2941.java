c5dd4453ca81dd66df73aaab091e6194838eb532
woosh1108 <woosh1108@naver.com>
2023-09-10 10:15:03 +0900

    백준 실버5 2941 크로아티아 알파벳

--- src/sanghee/크로아티아알파벳_2941.java
+++ src/sanghee/크로아티아알파벳_2941.java
@@ -16,20 +16,11 @@ public class 크로아티아알파벳_2941 {
 
         // 반복문을 사용해 크로아티아 알파벳이 몇 개 들어있는지 세기
         for(int i=0; i<target.length; i++){
-            croatianAlphabet(word, target[i]); // 크로아티아 알파벳이 몇 개 들어있는지 확인하기
-            word = word.replace(target[i], "1"); // 확인한 크로아티아 알파벳을 바꿔주기
+            word = word.replace(target[i], "1"); // 크로아티아 알파벳을 바꿔주기
         }
 
         // 출력
         System.out.println(word.length());
 
-    }
-    public static void croatianAlphabet(String word, String target) {
-        int position = word.indexOf(target); // 크로아티아 알파벳의 첫 위치
-
-        // 여러 개의 크로아티아 알파벳 찾기
-        while (position != -1) {
-            position = word.indexOf(target, position + 1); // 다음 위치를 찾기 위해 이전 위치 이후부터 검색
-        }
     }
 }
