1f850d5f93a4bb01a3a33a753c65f8bec9239fd2
woosh1108 <woosh1108@naver.com>
2023-09-12 15:39:34 +0900

    백준 실버4 18258 큐2

--- src/sanghee/리모컨_1107.java
+++ src/sanghee/리모컨_1107.java
@@ -1,57 +1,76 @@
 package sanghee;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 리모컨_1107 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String N = sc.next();
-        int M = sc.nextInt();
-        int getBroken[] = new int[10];
+        // 이동하려는 채널 N
+        int N = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<M; i++){
-            getBroken[sc.nextInt()] = -1;
-        }
+        // 고장난 버튼의 개수 M
+        int M = Integer.parseInt(br.readLine());
 
-        if(N.equals("100"))
-            System.out.println(0);
-        else {
-            int min = Integer.MAX_VALUE;
-            String v = "";
-            String closer = "";
+        // 고장난 버튼을 저장하는 배열
+        boolean[] isBroken = new boolean[10];
 
-            for (int i = 0; i < 1000000; i++) {
-                boolean isOk = true;
-                v = i + "";
-                for (int j = 0; j < v.length(); j++) {
-                    if (getBroken[v.charAt(j) - '0'] == -1) {
-                        isOk = false;
-                        break;
-                    }
-                }
-                if (isOk) {
-                    if (min > Math.abs(Integer.parseInt(N) - Integer.parseInt(v))) {
-                        min = Math.abs(Integer.parseInt(N) - Integer.parseInt(v));
-                        closer = v;
-                    }
-                }
-            }
-
-            int result1 = Math.abs(Integer.parseInt(N) - 100);
-            if (closer.equals("")){
-                System.out.println(result1);
-            }
-            else {
-                int result2 = Math.abs(Integer.parseInt(N) - Integer.parseInt(closer)) + closer.length();
-                if (result1 > result2){
-                    System.out.println(result2);
-                }
-                else{
-                    System.out.println(result1);
-                }
+        // 고장난 버튼을 true로 저장해주기
+        if (M > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) {
+                int brokenButton = Integer.parseInt(st.nextToken());
+                isBroken[brokenButton] = true;
             }
         }
 
+        // 초기값은 100번에서 시작
+        int currentChannel = 100;
+        int result = Math.abs(currentChannel - N); // 초기값으로 + 또는 - 버튼만을 사용하는 경우
+
+        // 0부터 1,000,000까지 순회하면서 최소 횟수 계산
+        for (int channel = 0; channel <= 1000000; channel++) {
+            if (isPossible(channel, isBroken)) { // 만약 고장난 버튼이 없으면
+                int press = Math.abs(channel - N) + getPressCount(channel);
+                result = Math.min(result, press);
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    // 고장난 버튼을 사용할 수 있는지 확인하는 함수
+    private static boolean isPossible(int channel, boolean[] isBroken) {
+        if (channel == 0) { //
+            return !isBroken[0];
+        }
+
+        while (channel > 0) {
+            int digit = channel % 10; // 채널의 마지막 숫자 추출하여 digit에 저장
+            if (isBroken[digit]) { // 현재 숫자가 망가진 숫자인지 검사
+                return false;
+            }
+            channel /= 10; // 다음 자리로 이동
+        }
+
+        return true; // 모든 자리를 검사했으면 이동가능한 채널로 판단하고 true를 반환
+    }
+
+    // 채널로 이동하기 위해 눌러야 하는 버튼 횟수를 계산하는 함수
+    private static int getPressCount(int channel) {
+        if (channel == 0) {
+            return 1;
+        }
+
+        int count = 0; // 버튼을 누른 횟수
+        while (channel > 0) {
+            count++;
+            channel /= 10; // 다음 자리로 이동
+        }
+
+        return count;
     }
 }
---
+++ src/sanghee/큐2_18258.java
@@ -0,0 +1,65 @@
+package sanghee;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class 큐2_18258 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 명령어를 입력받을 개수
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<String> queue = new ArrayDeque<>(); // 숫자를 저장할 Queue
+        StringBuilder output = new StringBuilder(); // 출력할 내용
+
+        for(int i=0; i<N; i++){
+            // 명령어를 입력받아 공백 단위로 배열로 저장하기
+            String input = br.readLine();
+            String[] tokens = input.split(" ");
+
+            switch (tokens[0]){
+                case "push": // 입력받은 정수를 큐에 넣기
+                    queue.offer(tokens[1]);
+                    break;
+                case "pop": // 큐에서 가장 앞에 있는 정수를 빼고 그 수를 출력한다. 큐가 비어있다면 -1을 출력한다.
+                    if(!queue.isEmpty()){
+                        output.append(queue.poll()+"\n");
+                    }else {
+                        output.append("-1\n");
+                    }
+                    break;
+                case "size": // 큐에 들어있는 정수의 개수를 출력한다.
+                    output.append(queue.size()+"\n");
+                    break;
+                case "empty": // 큐가 비어있으면 1, 아니면 0을 출력한다.
+                    if(queue.isEmpty()){
+                        output.append("1\n");
+                    }else {
+                        output.append("0\n");
+                    }
+                    break;
+                case "front": // 큐의 가장 앞에 있는 정수를 출력한다. 큐가 비어있으면 -1을 출력한다.
+                    if(!queue.isEmpty()){
+                        output.append(queue.peek()+"\n");
+                    }else {
+                        output.append("-1\n");
+                    }
+                    break;
+                case "back": // 큐의 가장 뒤에 있는 정수를 출력한다. 큐가 비어있으면 -1을 출력한다.
+                    if(!queue.isEmpty()){
+                        output.append(queue.peekLast()+"\n");
+                    }else {
+                        output.append("-1\n");
+                    }
+                    break;
+
+            }
+        }
+
+        // 한꺼번에 출력하기
+        System.out.print(output);
+    }
+}
