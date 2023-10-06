40d48d0d3e90137bc4f060564d08c4b92cdcfe12
woosh1108 <woosh1108@naver.com>
2023-10-06 19:04:45 +0900

    백준 브론즈2 2745 진법 변환

--- src/sanghee/진법변환_2745.java
+++ src/sanghee/진법변환_2745.java
@@ -3,8 +3,6 @@ package sanghee;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.Scanner;
 
 public class 진법변환_2745 {
     public static void main(String[] args) throws IOException {
