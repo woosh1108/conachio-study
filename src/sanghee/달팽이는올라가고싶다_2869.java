bbff382b52e44ac8b36077f427f1886415e7ec80
woosh1108 <woosh1108@naver.com>
2023-10-19 00:27:44 +0900

    백준 브론즈1 2869 달팽이는 올라가고 싶다

---
+++ src/sanghee/달팽이는올라가고싶다_2869.java
@@ -0,0 +1,27 @@
+package sanghee;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 달팽이는올라가고싶다_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        String[] ABV = input.split(" ");
+
+        int days = 0;
+
+        int v = Integer.parseInt(ABV[2])-Integer.parseInt(ABV[0]);
+        int c = Integer.parseInt(ABV[0])-Integer.parseInt(ABV[1]);
+
+        if(v%c==0){
+            days = v/c+1;
+        } else{
+            days = v/c+2;
+        }
+
+        System.out.println(days);
+    }
+}
