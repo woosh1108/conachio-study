4f9fdefb731d480c2f06a94c8d390fbc88a8747b
woosh1108 <woosh1108@naver.com>
2023-10-24 21:29:14 +0900

    백준 브론즈4 24263 알고리즘의 수행 시간2

---
+++ src/sanghee/알고리즘의수행시간2_24263.java
@@ -0,0 +1,15 @@
+package sanghee;
+
+import java.util.Scanner;
+
+public class 알고리즘의수행시간2_24263 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // n 입력받기
+
+        // 출력
+        System.out.println(n); // MenOfPassion 알고리즘의 수행 횟수
+        System.out.println(1); // MenOfPassion 알고리즘의 수행 횟수의 최고차항의 차수
+    }
+}
